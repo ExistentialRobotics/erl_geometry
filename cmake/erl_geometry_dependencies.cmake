4a90b7599ef0560e7b59d5f60c42e33aad05108d
Zhirui Dai <daizhirui@hotmail.com>
2025-07-20 01:28:10 -0700

    move ROS things to other repos

--- cmake/erl_geometry_dependencies.cmake
+++ cmake/erl_geometry_dependencies.cmake
@@ -1,3 +1,5 @@
+# if (NOT COMMAND erl_project_setup) find_package(erl_cmake_tools REQUIRED) endif ()
+
 erl_config_nanoflann()
 erl_config_qhull()
 erl_config_absl()
@@ -6,18 +8,5 @@ if (ROS1_ACTIVATED)
     erl_config_qt5(Core Widgets)
     erl_config_ogre()
 elseif (ROS2_ACTIVATED)
-    erl_config_qt(Core Widgets)  # Qt5 or Qt6
-endif ()
-
-if (ERL_BUILD_TEST_${PROJECT_NAME})
-    add_subdirectory(deps/octomap) # test_occupancy_octree_impls depends on octomap
-    erl_config_cgal()
-
-    if (NOT CGAL_FOUND)
-        erl_ignore_gtest(test_convex_hull_3d_impls.cpp)
-        message(WARNING "CGAL not found, ignoring test_convex_hull_3d_impls")
-    endif ()
-
-    erl_set_gtest_extra_libraries(test_convex_hull_3d_impls CGAL::CGAL)
-    erl_set_gtest_extra_libraries(test_occupancy_octree_impls octomap)
+    erl_config_qt(Core Widgets) # Qt5 or Qt6
 endif ()
--- docker/build.bash
+++ docker/build.bash
@@ -2,9 +2,7 @@
 set -e
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 BUILD_UBUNTU_2004=${BUILD_UBUNTU_2004:-true}
-BUILD_ROS_NOETIC=${BUILD_ROS_NOETIC:-false}
 BUILD_UBUNTU_2204=${BUILD_UBUNTU_2204:-true}
-BUILD_ROS_HUMBLE=${BUILD_ROS_HUMBLE:-false}
 
 if [ "${BUILD_UBUNTU_2004}" = "true" ]; then
   echo "Building Ubuntu 20.04 image..."
@@ -12,20 +10,8 @@ if [ "${BUILD_UBUNTU_2004}" = "true" ]; then
   ./build.bash $@
 fi
 
-if [ "${BUILD_ROS_NOETIC}" = "true" ]; then
-  echo "Building ROS image..."
-  cd $SCRIPT_DIR/ros-noetic
-  ./build.bash $@
-fi
-
 if [ "${BUILD_UBUNTU_2204}" = "true" ]; then
   echo "Building Ubuntu 22.04 image..."
   cd $SCRIPT_DIR/ubuntu-2204
-  ./build.bash $@
-fi
-
-if [ "${BUILD_ROS_HUMBLE}" = "true" ]; then
-  echo "Building ROS Humble image..."
-  cd $SCRIPT_DIR/ros-humble
   ./build.bash $@
 fi
