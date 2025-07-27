1ac2d0078a34b02bd4601c16a9560f8ef14f0369
Zhirui Dai <daizhirui@hotmail.com>
2025-07-26 18:07:18 -0700

    clean up

--- scripts/setup_ubuntu_22.04_24.04.bash
+++ scripts/setup_ubuntu_22.04_24.04.bash
@@ -3,6 +3,9 @@
 mkdir tmp
 cd tmp
 
+
+sudo apt update && sudo apt install -y libabsl-dev libqhull-dev
+
 # install nanoflann
 git clone https://github.com/jlblancoc/nanoflann && \
 cd nanoflann && git checkout v1.7.1 && \
@@ -11,24 +14,6 @@ cmake .. -DCMAKE_BUILD_TYPE=Release && \
 make install && \
 cd ../.. && \
 rm -rf nanoflann
-
-# install abseil-cpp
-git clone --recursive https://github.com/abseil/abseil-cpp.git && cd abseil-cpp && \
-git checkout 20240722.1 && mkdir -p build && cd build && \
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="${CXXFLAGS} -DNDEBUG" \
-   -DCMAKE_CXX_STANDARD=17 -DBUILD_SHARED_LIBS=ON -DABSL_PROPAGATE_CXX_STD=ON && \
-make -j`nproc` && make install && \
-cd ../.. && \
-rm -rf abseil-cpp
-
-# install qhull
-git clone --recursive https://github.com/qhull/qhull.git && cd qhull && \
-git checkout 2020.2 && mkdir -p my_build && cd my_build && \
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-fPIC -ffat-lto-objects" \
-  -DCMAKE_C_FLAGS="-fPIC -ffat-lto-objects" -DCMAKE_SKIP_RPATH=ON && \
-make -j`nproc` && make install && \
-cd ../.. && \
-rm -rf qhull
 
 # Open3D 0.19.0 requires at least CMake 3.24
 wget https://github.com/Kitware/CMake/releases/download/v3.24.0/cmake-3.24.0-linux-x86_64.sh &&\
